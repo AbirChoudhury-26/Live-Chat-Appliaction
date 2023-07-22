7b0663f6766f78efa688cf4f0bdf83e6cfeedbb9
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-07-22 19:56:41 +0530

    Servre Page added

---
+++ storebackend/server.js
@@ -0,0 +1,55 @@
+// server.js
+import express from 'express';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+
+dotenv.config();
+const app = express();
+
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+
+
+
+// Body parsing middlewarec
+app.use(express.json());
+
+// Signup route
+app.post('/api/signup', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error('Signup error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${port}`);
+});
