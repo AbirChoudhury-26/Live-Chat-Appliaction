cdaadb5913a34bf7459faa3291b83747edbc281e
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-07-24 23:31:59 +0530

    SignIn added

--- storebackend/Routes/userRoutes.js
+++ storebackend/Routes/userRoutes.js
@@ -30,3 +30,39 @@ const app=express();
     }
   });
   
+
+  app.post('/api/signin', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      // Check if user exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      // Compare the provided password with the hashed password in the database
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+  
+      // If the password is correct, you can implement a session or token-based authentication here
+      // For example, you can use JWT (JSON Web Tokens) to generate and send a token to the client for further authentication.
+  
+      res.status(200).json({ message: 'Signin successful', user });
+    } catch (error) {
+      console.error('Signin error:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  export default app;
+  
+  
+  
+  
+  
+  
+  
+  
