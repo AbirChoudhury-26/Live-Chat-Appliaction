dd6bc0dfb190e5e0ad02b2e85dd1a20239b150c0
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-09-22 23:42:58 +0530

    New

--- src/Room.js
+++ src/Room.js
@@ -8,7 +8,7 @@ const Room = () => {
 
   const meeting=async(element)=>{
     const appID=1962319122;
-     const serverSecret="ea83eae97e52a2d180d42aa6dffb5850";
+     const serverSecret=process.env.SERVERSECRET;
      const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  Date.now().toString(),"abir");
 
      const zp = ZegoUIKitPrebuilt.create(kitToken);
@@ -24,7 +24,7 @@ const Room = () => {
 
 
   return (
-    <div ref={meeting} style={{ width:"100vw", height:"100vh"}}>
+    <div ref={meeting} style={{ width:"200vw", height:"200vh"}}>
       <h1>Room Page</h1>
     </div>
   )
